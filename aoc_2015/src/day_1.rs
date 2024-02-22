028c2a99b0c04f83f785a6c806e523761737e36e
Kirubel Midru <kirubelmidru@gmail.com>
2024-02-22 22:33:07 +0300

    day_one part_two added

--- aoc_2015/src/day_1.rs
+++ aoc_2015/src/day_1.rs
@@ -2,13 +2,31 @@ use std::fs;
 
 pub fn day_1() {
     let mut floor: i32 = 0;
+    let mut count: i32 = 0;
     let input: String = fs::read_to_string("input/day_1.txt").unwrap();
+    
     for i in input.chars() {
         match i {
             '(' => floor += 1,
             ')' => floor -= 1,
             _ => {} 
         };
+    };
+
+    println!("part one: {floor}");
+
+    floor = 0;
+
+    for i in input.chars() {
+        match i {
+            '(' => floor += 1,
+            ')' => floor -= 1,
+            _ => {}
+        };
+        count += 1;
+        if floor == -1 {
+            println!("part two: {count}");
+            break;
+        }
     }
-    println!("{floor}");
 }
