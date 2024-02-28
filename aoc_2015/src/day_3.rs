2787cec7caf048cd83fde15d182e6442295ae828
Kirubel Midru <kirubelmidru@gmail.com>
2024-02-28 23:12:43 +0300

    day_3 part_2 and day_4 file added

--- aoc_2015/src/day_3.rs
+++ aoc_2015/src/day_3.rs
@@ -23,4 +23,43 @@ pub fn day_3() {
         }
     }
     println!("{count}");
+    
+    santa_curr[0] = 0;
+    santa_curr[1] = 0;
+    count = 1;
+    let mut iter: i32 = 1;
+    for i in input.chars() {
+        match iter % 2 {
+            0 => {
+                match i{
+                '>' => santa_curr[1] += 1,
+                '<' => santa_curr[1] -= 1,
+                '^' => santa_curr[0] += 1,
+                'v' => santa_curr[0] -= 1,
+                _ => {},
+                };
+                iter += 1;
+                if !visited.contains(&santa_curr) {
+                    count += 1;
+                    visited.insert(santa_curr.clone());
+                }
+            },
+            1 => {
+                match i {
+                    '>' => robo_curr[1] += 1,
+                    '<' => robo_curr[1] -= 1,
+                    '^' => robo_curr[0] += 1,
+                    'v' => robo_curr[0] -= 1,
+                    _ => {},
+                };
+                iter += 1;
+                if !visited.contains(&robo_curr) {
+                    count += 1;
+                    visited.insert(robo_curr.clone());
+                }
+            },
+            _ => {}
+        }
+    }
+    println!("{count}");
 }
--- aoc_2015/src/main.rs
+++ aoc_2015/src/main.rs
@@ -1,8 +1,10 @@
 mod day_1;
 mod day_3;
+mod day_4;
 
 use day_1::day_1;
 use day_3::day_3;
+use day_4::day_4;
 
 fn main() {
     day_3();
